a803785a3d6d12a9c5897c8b0d13cf04fc6b604c
chepecarlos <chepecarlos@alswblog.org>
2023-03-29 11:10:50 -0600

    credenciales por defecto

---
+++ Arduino/Subs/data_ejemplo.h
@@ -0,0 +1,11 @@
+//Configuraciones de RED
+const char* ssid_1     = "xxxx";
+const char* password_1 = "xxx";
+const char* ssid_2     = "xxx";
+const char* password_2 = "xxx";
+
+const char* nombre = "SuperSub";
+
+const char* BrokerMQTT = "test.mosquitto.org";
+const char* TopicMQTT = "subreal";
+const int PuertoMQTT = 1883;
